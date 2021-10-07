ba5e6e6bd80959ff58be7aa1ce349ef89c6ddaf0
professorisidro <isidro@professorisidro.com.br>
2021-10-06 21:47:34 -0300

    Adicionando endpoint raiz

---
+++ src/main/java/br/com/professorisidro/fateczl/controller/HelloController.java
@@ -0,0 +1,16 @@
+package br.com.professorisidro.fateczl.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+public class HelloController {
+	
+	@GetMapping("/")
+	public String sayHello() {
+		return "Bem vindos ao exemplo do CI/CD na Fatec ZL - By Professor Isidro";
+	}
+
+}
